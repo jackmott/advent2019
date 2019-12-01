6ebcbb024d1724af69225b4a2649ce43657c78b5
Jack Mott <jack.mott@gmail.com>
2019-12-01 10:19:22 -0600

    cleanup lib

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,25 +1,27 @@
 use clipboard::*;
 use std::fs::File;
-use std::io::{Lines,prelude::*, BufReader};
+use std::io::{prelude::*, BufReader};
 use std::str::FromStr;
 
-pub fn read_file(file_name:&str)  -> Lines<BufReader<File>> {
+// Return an iterator over each line of a file
+pub fn read_file(file_name:&str)  -> impl Iterator<Item = String> {
     let file = File::open(file_name).expect("could not open file");
     let reader = BufReader::new(file);
-    reader.lines()
+    reader.lines().map(|line| match line {
+        Ok(line) => line,
+        Err(_) => panic!("unable to read line")
+    }   
+    )
 }  
 
+// Load a file and return an iterator of parseable types, eg a file with an int on each line
 pub fn read_fromstr<T>(file_name:&str) -> impl Iterator<Item = T>
 where T: FromStr, <T as FromStr>::Err : std::fmt::Debug
 {
-    read_file(file_name).map(|line_result| {
-        match line_result {
-            Ok(line) => line.parse::<T>().unwrap(),
-            Err(_) => panic!("failed to read line")
-        }
-    })
+    read_file(file_name).map(|line| line.parse::<T>().unwrap())
 } 
 
+/// Put a string on the system clipboard
 pub fn clip(s:String) {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     ctx.set_contents(s.to_owned()).unwrap();

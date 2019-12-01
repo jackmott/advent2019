18317da77f30d724105b06d7b818d4e4f869dbbb
Jack Mott <jack.mott@gmail.com>
2019-12-01 10:16:56 -0600

    cleanup lib

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -3,16 +3,16 @@ use utils::*;
 fn calc_fuel(total:i64,mass:i64) -> i64 {
     let fuel = mass/3-2;
     if fuel <= 0 {
-        return total;
+        total
     } else {
-        return calc_fuel(total+fuel,fuel);
+        calc_fuel(total+fuel,fuel)
     }
 }
 
 fn main() {
     let result : i64 = read_fromstr::<i64>("input.txt").map(|mass| mass/3-2).sum();
     println!("{}",result);
-    let result : i64 = read_fromstr::<i64>("input.txt").map(|mass| calc_fuel(0,mass)).sum();
+    let result : i64 = read_fromstr("input.txt").map(|mass| calc_fuel(0,mass)).sum();
     println!("{}",result);
     clip(result.to_string());
 }

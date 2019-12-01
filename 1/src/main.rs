b292efc5b4b574f23cbf3a60365973aef9a12779
Jack Mott <jack.mott@gmail.com>
2019-12-01 08:18:54 -0600

    generalized read lines

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -10,9 +10,9 @@ fn calc_fuel(total:i64,mass:i64) -> i64 {
 }
 
 fn main() {
-    let result : i64 = read_ints("input.txt").map(|mass| mass/3-2).sum();
+    let result : i64 = read_fromstr::<i64>("input.txt").map(|mass| mass/3-2).sum();
     println!("{}",result);
-    let result : i64 = read_ints("input.txt").map(|mass| calc_fuel(0,mass)).sum();
+    let result : i64 = read_fromstr::<i64>("input.txt").map(|mass| calc_fuel(0,mass)).sum();
     println!("{}",result);
     clip(result.to_string());
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,7 @@
 use clipboard::*;
 use std::fs::File;
 use std::io::{Lines,prelude::*, BufReader};
+use std::str::FromStr;
 
 pub fn read_file(file_name:&str)  -> Lines<BufReader<File>> {
     let file = File::open(file_name).expect("could not open file");
@@ -8,10 +9,12 @@ pub fn read_file(file_name:&str)  -> Lines<BufReader<File>> {
     reader.lines()
 }  
 
-pub fn read_ints(file_name:&str) -> impl Iterator<Item = i64>{
+pub fn read_fromstr<T>(file_name:&str) -> impl Iterator<Item = T>
+where T: FromStr, <T as FromStr>::Err : std::fmt::Debug
+{
     read_file(file_name).map(|line_result| {
         match line_result {
-            Ok(line) => line.parse::<i64>().unwrap(),
+            Ok(line) => line.parse::<T>().unwrap(),
             Err(_) => panic!("failed to read line")
         }
     })

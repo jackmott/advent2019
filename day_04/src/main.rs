88adb006517ee8843cea53c75d0d8629e52f9709
Jack Mott <jack.mott@gmail.com>
2019-12-04 16:16:44 -0600

    format

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -1,13 +1,13 @@
-use std::time::{Duration, Instant};
-use std::cell::{RefCell,Ref};
+use std::cell::{Ref, RefCell};
+use std::time::Instant;
 
 struct Digits<'a> {
     digits: &'a RefCell<Vec<u8>>,
-    first:bool,
+    first: bool,
 }
 
-fn new_digits(num: i64) -> Vec<u8> {    
-    let mut digits : Vec<u8> = Vec::new();
+fn new_digits(num: i64) -> Vec<u8> {
+    let mut digits: Vec<u8> = Vec::new();
     let mut n = num;
     while n > 0 {
         digits.push((n % 10) as u8);
@@ -16,11 +16,10 @@ fn new_digits(num: i64) -> Vec<u8> {
     digits.reverse();
     digits
 }
-      
 
-fn to_num(digits:&[u8]) -> i64 {
+fn to_num(digits: &[u8]) -> i64 {
     let mut num = 0;
-    let mut mul = 1;    
+    let mut mul = 1;
     for i in (0..digits.len()).rev() {
         num = num + (digits[i] as i64) * mul;
         mul *= 10;
@@ -28,28 +27,28 @@ fn to_num(digits:&[u8]) -> i64 {
     num
 }
 
-fn is_valid1(digits:&[u8]) -> bool {
-    let mut digits_count: [i64; 10] = [0; 10];    
+fn is_valid1(digits: &[u8]) -> bool {
+    let mut digits_count: [i32; 10] = [0; 10];
     for d in digits {
         digits_count[*d as usize] += 1;
     }
     digits_count.iter().any(|count| *count >= 2)
 }
 
-fn is_valid2(digits:&[u8]) -> bool {
-    let mut digits_count: [i64; 10] = [0; 10];    
+fn is_valid2(digits: &[u8]) -> bool {
+    let mut digits_count: [i32; 10] = [0; 10];
     for d in digits {
         digits_count[*d as usize] += 1;
     }
     digits_count.iter().any(|count| *count == 2)
 }
 impl<'a> Iterator for Digits<'a> {
-    type Item = Ref<'a,[u8]>;
+    type Item = Ref<'a, [u8]>;
 
-    fn next(&mut self) -> Option<Ref<'a,[u8]>> {        
+    fn next(&mut self) -> Option<Ref<'a, [u8]>> {
         if self.first {
             self.first = false;
-            return Some(Ref::map(self.digits.borrow(),Vec::as_slice));
+            return Some(Ref::map(self.digits.borrow(), Vec::as_slice));
         }
         let mut digits = self.digits.borrow_mut();
         let mut i = digits.len() - 1;
@@ -60,7 +59,7 @@ impl<'a> Iterator for Digits<'a> {
                     digits[j] = digits[i];
                 }
                 drop(digits);
-                return Some(Ref::map(self.digits.borrow(),Vec::as_slice));
+                return Some(Ref::map(self.digits.borrow(), Vec::as_slice));
             } else {
                 if i == 0 {
                     break;
@@ -75,17 +74,23 @@ impl<'a> Iterator for Digits<'a> {
 
 fn main() {
     let now = Instant::now();
-    let answer1 = Digits{ digits:&RefCell::new(new_digits(388888)), first:true}
-        .filter(|d| is_valid1(d))
-        .take_while(|d| to_num(d) <= 843167)
-        .count();
+    let answer1 = Digits {
+        digits: &RefCell::new(new_digits(388888)),
+        first: true,
+    }
+    .filter(|d| is_valid1(d))
+    .take_while(|d| to_num(d) <= 843167)
+    .count();
     println!("answer1:{}", answer1);
 
-    let answer2 =  Digits{ digits:&RefCell::new(new_digits(388888)), first:true }
-        .filter(|d| is_valid2(d))
-        .take_while(|d| to_num(d) <= 843167)
-        .count();
+    let answer2 = Digits {
+        digits: &RefCell::new(new_digits(388888)),
+        first: true,
+    }
+    .filter(|d| is_valid2(d))
+    .take_while(|d| to_num(d) <= 843167)
+    .count();
     println!("answer:{}", answer2);
 
-    println!("{}",now.elapsed().as_millis());
+    println!("{}", now.elapsed().as_millis());
 }

00c10cb56b85ad1ed4395d0c65fde35da98fad50
Jack Mott <jack.mott@gmail.com>
2019-12-07 07:09:35 -0600

    cleanup day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,56 +1,52 @@
 use utils::*;
 
-fn run_computer(digits: &mut [i64]) -> i64 {
-    for i in (0..digits.len()).step_by(4) {
-        if digits[i] == 99 {
-            break;
-        } else {
-            
-            let a = digits[digits[i + 1] as usize];
-            let b = digits[digits[i + 2] as usize];
-            
-            digits[digits[i + 3] as usize] = 
-                if digits[i] == 1 {
-                    a + b
-                } else if digits[i] == 2 {
-                    a * b
-                } else {
-                    panic!("error");
-                };
-        }
-    }
-    digits[0]
+struct SuperComputer {
+    digits:Vec<i64>,
 }
 
-fn part1() {
-    let mut digits: Vec<i64> =  read_file("input.txt")
-        .nth(0)
-        .unwrap()        
-        .split(',')
-        .map(|s| s.parse::<i64>().unwrap())
-        .collect();
+impl SuperComputer {
+    fn new(digits:&Vec<i64>) -> SuperComputer {
+        SuperComputer {
+            digits:digits.clone(),
+        }
+    }
 
-    let r = run_computer(&mut digits);
+    fn run(&mut self) -> i64 {
+        for i in (0..self.digits.len()).step_by(4) {
+            if self.digits[i] == 99 {
+                break;
+            } else {
+                let p1 = self.digits[i+1];
+                let p2 = self.digits[i+2];
+                let p3 = self.digits[i+3];
+                let a = self.digits[p1 as usize];
+                let b = self.digits[p2 as usize];
+                self.digits[p3 as usize] = match self.digits[i] {
+                    1 => a+b,
+                    2 => a*b,
+                    _ => panic!("error")
+                };
+            }
+        }
+        self.digits[0]
+    }
+}
+
+fn part1(digits:&Vec<i64>) {
+    let r = SuperComputer::new(digits).run();
     println!("{}",r);
     clip(format!("{}",r));
 }
 
 
-fn part2() {
-    let original_digits: Vec<i64> =  read_file("input.txt")
-    .nth(0)
-    .unwrap()        
-    .split(',')
-    .map(|s| s.parse::<i64>().unwrap())
-    .collect();
-
+fn part2(digits:&Vec<i64>) {
     for noun in 0..100 {
         for verb in 0..100 {
-            let mut digits = original_digits.clone();
+            let mut digits = digits.clone();
             digits[1] = noun;
-            digits[2] = verb;           
-            if run_computer(&mut digits) == 19690720 {
-                println!("{} {}",noun,verb);
+            digits[2] = verb;
+            if SuperComputer::new(&digits).run() == 19690720 {
+                println!("{}",100*noun+verb);
                 clip(format!("{}",100*noun+verb));
             }
         }
@@ -59,6 +55,12 @@ fn part2() {
 }
 
 fn main() {
-    part1();
-    part2();
+    let digits: Vec<i64> =  read_file("input.txt")
+        .nth(0)
+        .unwrap()
+        .split(',')
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+    part1(&digits);
+    part2(&digits);
 }

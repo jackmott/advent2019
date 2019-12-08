638ccac5863ea4e7fed498d6a0ee53b18ba3ebaa
Jack Mott <jack.mott@gmail.com>
2019-12-07 20:19:54 -0600

    final answer, maybe

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,8 +1,10 @@
 use std::sync::mpsc::channel;
 use std::sync::mpsc::{Receiver, SendError, Sender};
 use std::thread;
+use std::collections::VecDeque;
 use utils::*;
 
+#[allow(dead_code)]
 struct SuperComputer {
     name: String,
     digits: Vec<i64>,
@@ -237,54 +239,50 @@ fn main() -> Result<(), SendError<i64>> {
         .map(|s| s.parse::<i64>().unwrap())
         .collect();
 
-    // part 1
+    // PART 1
+
+    // Phase Settings
     let nums: Vec<i64> = vec![0, 1, 2, 3, 4];
     let perms = perm(nums);
 
     let mut max = -999999;
+    // For each permutation of phase settings
     for perm in perms {
-        let digits_a = digits.clone();
-        let digits_b = digits.clone();
-        let digits_c = digits.clone();
-        let digits_d = digits.clone();
-        let digits_e = digits.clone();
 
+        // Start off the computation with perm[0] and 0 as inputs
         let (a_send, a_in) = channel();
         a_send.send(perm[0])?;
         a_send.send(0)?;
-        let (a_out, a_recv) = channel();
-        a_out.send(perm[1])?;
-        thread::spawn(move || {
-            SuperComputer::new("A".to_string(), digits_a, a_out, a_in).run();
-        });
 
-        let (b_out, b_recv) = channel();
-        b_out.send(perm[2])?;
-        thread::spawn(move || {
-            SuperComputer::new("B".to_string(), digits_b, b_out, a_recv).run();
-        });
-
-        let (c_out, c_recv) = channel();
-        c_out.send(perm[3])?;
-        thread::spawn(move || {
-            SuperComputer::new("C".to_string(), digits_c, c_out, b_recv).run();
-        });
-
-        let (d_out, d_recv) = channel();
-        d_out.send(perm[4])?;
-        thread::spawn(move || {
-            SuperComputer::new("D".to_string(), digits_d, d_out, c_recv).run();
-        });
-
-        let (e_out, e_recv) = channel();
-        thread::spawn(move || {
-            SuperComputer::new("E".to_string(), digits_e, e_out, d_recv).run();
-        });
-
-        match e_recv.recv() {
-            Ok(e) => {
-                if e > max {
-                    max = e
+        // Build up input and output channels for each Amplifier
+        let mut out_channels = VecDeque::<Sender<i64>>::new();
+        let mut recv_channels = VecDeque::<Receiver<i64>>::new();
+        recv_channels.push_back(a_in);
+        for _ in 0 .. perm.len() {
+            let (out,recv) = channel();
+            out_channels.push_back(out);
+            recv_channels.push_back(recv);
+        }
+        // Spawn a thread for each amplifier
+        for i in 0 .. perm.len() {
+            let out = out_channels.pop_front().unwrap();
+            // Send the phase setting into the input of the NEXT machine
+            if i+1 < perm.len() {
+                out.send(perm[i+1])?;
+            }
+            // Set the input channel to the receiver of the PREVIOUS machine's output
+            let recv = recv_channels.pop_front().unwrap();
+            let digits_clone = digits.clone();
+            // Spawn a thread and run the computer
+            thread::spawn(move || {
+                SuperComputer::new(i.to_string(), digits_clone, out, recv).run();
+            });
+        }
+        // Get the output from the last am,plifier
+        match recv_channels.pop_back().unwrap().recv() {
+            Ok(last_amplifier_output) => {
+                if last_amplifier_output > max {
+                    max = last_amplifier_output;
                 }
             }
             Err(_) => panic!("we broke"),
@@ -292,54 +290,72 @@ fn main() -> Result<(), SendError<i64>> {
     }
     println!("max:{}", max);
 
-    // part2
+    // PART2
+
+    // Phase settings
     let nums: Vec<i64> = vec![5, 6, 7, 8, 9];
     let perms = perm(nums);
     max = -9999999;
 
+    // For each permutattion of phase settings
     for perm in perms {
-        let digits_a = digits.clone();
-        let digits_b = digits.clone();
-        let digits_c = digits.clone();
-        let digits_d = digits.clone();
-        let digits_e = digits.clone();
 
+        //Set up a channel to send the final output of the final amplifier
         let (result_send, result_recv) = channel();
-        let (a_out, a_recv) = channel();
-        a_out.send(perm[1])?;
-        let (b_out, b_recv) = channel();
-        b_out.send(perm[2])?;
-        let (c_out, c_recv) = channel();
-        c_out.send(perm[3])?;
-        let (d_out, d_recv) = channel();
-        d_out.send(perm[4])?;
-        let (e_out, e_recv) = channel();
-        e_out.send(perm[0])?;
-        e_out.send(0)?;
 
-        thread::spawn(move || {
-            SuperComputer::new("A".to_string(), digits_a, a_out, e_recv).run();
-        });
-        thread::spawn(move || {
-            SuperComputer::new("B".to_string(), digits_b, b_out, a_recv).run();
-        });
-        thread::spawn(move || {
-            SuperComputer::new("C".to_string(), digits_c, c_out, b_recv).run();
-        });
-        thread::spawn(move || {
-            SuperComputer::new("D".to_string(), digits_d, d_out, c_recv).run();
-        });
-        thread::spawn(move || {
-            let mut e = SuperComputer::new("E".to_string(), digits_e, e_out, d_recv);
-            e.run();
-            let _ = result_send.send(e.last_output.unwrap());
-        });
+        //Setup output and input channels for each amplifier
+        let mut out_channels = VecDeque::<Sender<i64>>::new();
+        let mut recv_channels = VecDeque::<Receiver<i64>>::new();
+        for i in 0 .. perm.len() {
+            let (out,recv) = channel();
+            // For the last machine, we put its recv channel at the front
+            // So that they become the input for the first machine
+            // And we send the starting input so that will go to the first machine
+            if i == perm.len()-1 {
+                out.send(perm[0])?;
+                out.send(0)?;
+                recv_channels.push_front(recv);
+            } else {
+                out.send(perm[i+1])?;
+                recv_channels.push_back(recv);
+            }
+            out_channels.push_back(out);
+        }
+        // Add the result channel to the end of the out channels
+        out_channels.push_back(result_send);
 
+        for i in 0 .. perm.len() {
+            let digits_clone = digits.clone();
+            let out = out_channels.pop_front().unwrap();
+            // If we are on the last machine, get the result sending channel out
+            // So we can move it into the thread
+            let result_sender =
+                if i == perm.len() - 1 {
+                    Some(out_channels.pop_front().unwrap())
+                } else {
+                    None
+                };
+            let recv = recv_channels.pop_front().unwrap();
+            // Spawn a thread for each amplifier
+            thread::spawn(move || {
+                // Create a computer and run it
+                let mut computer = SuperComputer::new(i.to_string(), digits_clone, out, recv);
+                computer.run();
+                // If we have a final result sender, send the last output of this cmplifier
+                match result_sender {
+                    Some(sender) => {let _ = sender.send(computer.last_output.unwrap());}
+                    None => ()
+                }
+            });
+        }
+
+        // Keep looping on the result sending channel until it dies
+        // keep track of the max
         loop {
             match result_recv.recv() {
-                Ok(e) => {
-                    if e > max {
-                        max = e
+                Ok(last_machine_output) => {
+                    if last_machine_output > max {
+                        max = last_machine_output
                     }
                 }
                 Err(_) => break,
